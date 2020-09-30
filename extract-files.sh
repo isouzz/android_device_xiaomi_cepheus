0486cfa58c656d524c98bfbba6518b9dcdcc9cf6
isouzz <enzoo.site@gmail.com>
2020-09-30 06:46:19 +0100

    cepheus: Adapt scripts for Arrow

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-REVENGE_ROOT="${MY_DIR}"/../../..
+ARROW_ROOT="${MY_DIR}"/../../..
 
-HELPER="${REVENGE_ROOT}/vendor/revengeos/build/tools/extract_utils.sh"
+HELPER="${ARROW_ROOT}/vendor/arrow/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -74,7 +74,7 @@ function blob_fixup() {
 }
 
 # Initialize the helper for common device
-setup_vendor "${DEVICE}" "${VENDOR}" "${REVENGE_ROOT}" true "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ARROW_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
